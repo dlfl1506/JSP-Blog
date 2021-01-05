2a4a812b39ef116899501bea8e227aa42882852a
YD <hoggaz@naver.com>
2021-01-05 16:56:07 +0900

    15. 로그인 구현

--- src/com/cos/blog/service/UserService.java
+++ src/com/cos/blog/service/UserService.java
@@ -1,22 +1,32 @@
 package com.cos.blog.service;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import com.cos.blog.config.DB;
 import com.cos.blog.domain.user.User;
+import com.cos.blog.domain.user.UserDao;
 import com.cos.blog.domain.user.dto.JoinReqDto;
 import com.cos.blog.domain.user.dto.LoginReqDto;
 import com.cos.blog.domain.user.dto.UpdateReqDto;
+import com.mysql.cj.xdevapi.PreparableStatement;
 
 public class UserService {
 
-	// 회원가입, 회원수정, 로그인, 아이디중복체크
+	private UserDao userDao; 
+	
+	public UserService() {
+		userDao = new UserDao();
+	}
 	
 	public int 회원가입(JoinReqDto dto) {
-		
-		return -1; 
+		int result = userDao.save(dto);
+		return result;
 	}
 	
 	public User 로그인(LoginReqDto dto) {
-		
-		return null;
+		return userDao.findByUsernameAndPassword(dto);
 	}
 	
 	public int 회원수정(UpdateReqDto dto) {
@@ -24,8 +34,8 @@ public class UserService {
 		return -1;
 	}
 
-	public int 아이디중복체크(String username) {
-		
-		return -1;
+	public int 유저네임중복체크(String username) {
+		int result = userDao.findByUsername(username);
+		return result;
 	}
 }
